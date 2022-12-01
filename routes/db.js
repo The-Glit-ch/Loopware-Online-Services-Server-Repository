36d4a2b24e57ebe7c945722f000046776f733c1a
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2022-12-01 16:14:55 -0500

    Minor Changes

--- routes/db.js
+++ routes/db.js
@@ -5,24 +5,24 @@
 const expres = require('express')
 const router = expres.Router()
 
-// Logger
-const logger = require('../custom/logger')
+// // Logger
+// const logger = require('../custom/logger')
 
-// MongoDB
-const { MongoClient } = require('mongodb')
-const uri = "mongodb://99.97.86.196:27017/?maxPoolSize=20&w=majority"
-
-
-// Middlware
-router.use(expres.json())
+// // MongoDB
+// const { MongoClient } = require('mongodb')
+// const uri = "mongodb://99.97.86.196:27017/?maxPoolSize=20&w=majority"
 
 
-// Save Data Endpoint
-router.post("/", (req, res) => {
-	console.log(req.body)
-	res.json({
-		"msg": "All good"
-	})
-})
+// // Middlware
+// router.use(expres.json())
+
+
+// // Save Data Endpoint
+// router.post("/", (req, res) => {
+// 	console.log(req.body)
+// 	res.json({
+// 		"msg": "All good"
+// 	})
+// })
 
 module.exports = router
