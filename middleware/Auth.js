cacb3d6dd31e298ae69df82e9b3583f461524fa8
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2022-11-30 19:31:03 -0500

    Update Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -7,18 +7,49 @@ const router = express.Router()
 // Crypto
 const crypto = require('crypto')
 
+// Filesystem
+const fs = require('fs')
+
 // Logger
 const logger = require('../custom/logger')
 
 
-// Set
+// Init Process
+function main(){
+	// Check if there is a token already saved
+	// If not generate a new token
 
+}
+
+function generate_new_keys(r_byte_limit){
+	let buffer = crypto.randomBytes(r_byte_limit)
+	let hash = crypto.createHash('sha256')
+	let private_key = buffer.toString('hex')
+	let public_key = hash.update(private_key).digest().toString('hex')
+
+	logger.formated_log(private_key)
+	logger.formated_log(public_key)
+}
+
+
+
+// Set
 
 router.use((req, res, next) => {
 	logger.formated_log("Connection")
-	crypto.Hash()
-	logger.formated_log(crypto.randomBytes(32))
+	// logger.formated_log(crypto.randomBytes(32))
 	next()
 })
+
+
+router.get("/api", (req, res) => {
+	let auth_header = req.headers.authorization
+	res.status(200).json({
+		"current_status": "alive",
+	})
+})
+
+generate_new_keys(32)
+
 
 module.exports = router
