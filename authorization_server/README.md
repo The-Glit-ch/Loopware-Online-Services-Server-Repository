8eac8879849bcc1d34644c3cbd58b7950fa07a23
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2022-12-08 19:31:11 -0500

    Clean up + documentation

--- authorization_server/README.md
+++ authorization_server/README.md
@@ -1,6 +1,31 @@
 # Loopware Online Subsystem - Authorization Server
 Contains HTTP(S) endpoints for handling user/server authentication
 
-## Endpoints
-- User		-> /auth/user
-- Server	-> /auth/server
+# Server Endpoints
+## (POST) || /auth/server/register
+Endpoint for registering game/app. Client ID must be passed in the Authorization header
+
+```rest
+POST http://localhost:8080/auth/server/register
+Authorization: Bearer MY_CLIENT_ID
+```
+
+---
+
+## (POST) || /auth/server/refresh
+Endpoint for refreshing access token. Refresh token must be passed in the Authorization header
+
+```rest
+POST http://localhost:8080/auth/server/refresh
+Authorization: Bearer MY_REFRESH_TOKEN
+```
+
+---
+
+## (POST) || /auth/server/logout
+Endpoint for logging out/invalidating access + refresh token. Refresh token must be passed in the Authorization header
+
+```rest
+POST http://localhost:8080/auth/server/logout
+Authorization: Bearer MY_REFRESH_TOKEN
+```
--- authorization_server/src/routes/server.ts
+++ authorization_server/src/routes/server.ts
@@ -10,17 +10,21 @@ import { validate_client_token, generate_new_jwt, verify_and_decode_jwt } from '
 // Token Storage
 var token_storage: Array<string> = new Array<string> // TODO: Add a Redis Cache server to hold refresh tokens
 
-// (POST) /auth/server
+// (POST) /auth/server/register
 // Register for a new access token. Client token must be provided for token to be granted
-router.post("/", (req, res) => {
+// || DONE ||
+router.post("/register", (req, res) => {
 	let auth_header: string |undefined = req.headers.authorization 					// Fetch the authorization header
 	let client_token: string | undefined = auth_header?.split(" ")[1] 				// Client token. Used for registration of a new access token
 	let server_access_token: string | undefined = process.env.AUTH_SERVER_ACCESS	// Fetch the server access token
 
+	// Client token provided?
 	if (client_token == undefined){ return res.status(400).json({"message": "No authorization token provided"}) }
-	if (server_token == undefined){ return res.status(500).json({"message": "Internal authorization error. Double check your cient token and internal server token"}) }
+	// Server access token available?
+	if (server_access_token == undefined){ return res.status(500).json({"message": "Internal authorization error. Double check your cient token and internal server token"}) }
 
-	if (validate_client_token(client_token, server_token)){
+	// Client valid? If so generate new keys
+	if (validate_client_token(client_token, server_access_token)){
 		let access_token: string = generate_new_jwt({token: client_token}, String(process.env.AUTH_SERVER_ACCESS), {expiresIn: '30s'}) 	// Can expire, Client Token + Server Access Token
 		let refresh_token: string = generate_new_jwt({token: client_token}, String(process.env.AUTH_SERVER_REFRESH)) 					// Dosent expire, CLient Token + Refresh Access Token
 
@@ -34,7 +38,8 @@ router.post("/", (req, res) => {
 })
 
 // (POST) "/auth/server/refresh"
-// Refresh access token
+// Refreshes the access token. Takes in the refresh JWT in the authorization header
+// || DONE ||
 router.post("/refresh", (req, res) => {
 	let auth_header: string | undefined = req.headers.authorization 				// Fetch the authorization header
 	let refresh_jwt: string | undefined = auth_header?.split(" ")[1] 				// Refresh Token = (Token: Client ID, Secret: Server Refresh Token)
@@ -64,42 +69,34 @@ router.post("/refresh", (req, res) => {
 
 })
 
-
-
 // (POST) /auth/server/logout
-// Logout the client from server access. Client must register for a new access token if they want to re-access data
+// Logouts the specified client. Must provide refresh token else will return a 501
+// || DONE(?) ||
 router.post("/logout", (req, res) => {
-	let auth_header: string | undefined = req.headers.authorization // 
-	let access_jwt: string | undefined = auth_header?.split(" ")[1] // Will be the refresh token
-	let server_refresh_token: string | undefined = process.env.AUTH_SERVER_REFRESH
+	let auth_header: string | undefined = req.headers.authorization 				// Fetch the authorization header
+	let refresh_jwt: string | undefined = auth_header?.split(" ")[1] 				// Will be the refresh token
+	let server_refresh_token: string | undefined = process.env.AUTH_SERVER_REFRESH 	// Fetch the server access toke
 
-	if (access_jwt == undefined){ return res.status(400).json({"message": "No token provided"}) }
+	// Refresh token provided?
+	if (refresh_jwt == undefined){ return res.status(400).json({"message": "No token provided"}) }
+	// Server refresh token available?
 	if (server_refresh_token == undefined){ return res.status(500).json({"message": "Internal authorization error. Double check your cient token and internal server token"}) }
 
-	let [success, data]: Array<any> = verify_and_decode_jwt(access_jwt, String(server_refresh_token))
+	// Is valid JWT? Return data if so
+	let [success, data]: Array<any> = verify_and_decode_jwt(refresh_jwt, String(server_refresh_token))
 
+	// Invalid JWT
 	if (!success){ return res.status(401).json({"message": "Invalid JWT"}) }
+	
+	// Invalid token in the JWT (this is an extreme case and prob will never happen)
 	if (!validate_client_token(data.token, String(process.env.SERVER_ACCESS_TOKEN))){ return res.status(401).json({"message": "Invalid token"}) }
-	if (!token_storage.includes(access_jwt)){ return res.status(400).json({"message": "Token no longer valid. Logout already done"}) }
+	
+	// Token in storage?
+	if (!token_storage.includes(refresh_jwt)){ return res.status(400).json({"message": "Token no longer valid. Logout already done"}) }
 
-	token_storage.splice(token_storage.indexOf(access_jwt), 1)
+	// Remove token from storage
+	token_storage.splice(token_storage.indexOf(refresh_jwt), 1)
 	res.status(200).json({"success": true, "message": "Logout successful"})
 })
-
-router.get("/check-access", (req, res) => {
-	let auth_header: string | undefined = req.headers.authorization
-	let client_jwt: string | undefined = auth_header?.split(" ")[1]
-	let server_access_token: string | undefined = process.env.AUTH_SERVER_ACCESS
-
-	if (client_jwt == undefined){ return res.status(400).json({"message": "No token provided"}) }
-	if (server_access_token == undefined){ return res.status(500).json({"message": "Internal authorization error. Double check your cient token and internal server token"}) }
-
-	let [success, data]: Array<any> = verify_and_decode_jwt(client_jwt, String(server_access_token))
-
-	if (!success){ return res.status(401).json({"message": "Token expired / Invalid token"}) }
-
-	res.status(200).json({"token_valid": success, "exp": data.exp, "iat": data.iat})
-})
-
 
 module.exports = router
