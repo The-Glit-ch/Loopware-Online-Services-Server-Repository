b416f5c05cf894a5f9a5b26ced9e31a5639c5454
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2022-12-21 22:10:50 -0500

    Unit testing | WIP

---
+++ authorization_server/tests/server-endpoints.test.ts
@@ -0,0 +1,37 @@
+// Imports
+import axios from 'axios'
+import { describe, test, expect } from 'vitest';
+
+// Docstring
+/**
+ * Loopware Online Subsystem @ Authorization Server - Server-Endpoint Tests || Contains tests for testing endpoints
+ * 🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯
+ */
+
+// Enums
+
+// Constants
+const PORT: string | undefined = process.env.PORT
+const BASE_URL: string = `http://127.0.0.1:${PORT}/auth/server`
+
+// Public Variables
+
+// Private Variables
+
+// _init()
+
+// Tests
+/**
+ * Test cases for "/register" endpoint
+ */
+describe('/register endpoint', () => {
+	test('Client token passed', () => {
+		let response = axios.get()
+	})
+
+
+
+
+})
+
+// Run
